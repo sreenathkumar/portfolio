1f17c58517bf16843b056d48f2fe813629d52611
Sreenath <sreenathkumar66@gmail.com>
2023-08-04 15:03:19 +0600

    feat: scroll top on click the logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,24 @@ import { useState } from "react";
 import "../assets/css/header.css";
 import logo from '../assets/images/logo.webp'
 import MobileMenu from "./ui/MobileMenu";
+import { smoothScroll } from "../utils/smoothScroll";
 
 export default function Header() {
    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
    const handleMobileMenu = () => {
       setMobileMenuOpen(!mobileMenuOpen);
    };
+   const scrollTop = () => {
+      smoothScroll(0)
+   }
    return (
       <header className="fixed w-full top-0 p-4 md:items-center ">
-         <div className="container flex justify-between items-center mx-auto md:px-12">
+         <div className="container flex justify-between items-center mx-auto md:px-12 cursor-pointer">
             <div className="logo">
                <img className="h-12"
                   src={logo}
                   alt="sreenath_kumar_logo"
+                  onClick={scrollTop}
                />
             </div>
             <nav>
